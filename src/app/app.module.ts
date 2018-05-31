0bd9b816b7c922637f907fab77581405a2582b14
deb <debkbanerji@gmail.com>
2018-05-30 21:16:48 -0700

    Import hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import 'hammerjs';
+
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from "@angular/router";
