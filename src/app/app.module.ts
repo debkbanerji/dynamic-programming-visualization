9cfcff9f699484fded21b89d29df6a841ac8cd40
deb <debkbanerji@gmail.com>
2018-06-01 00:54:00 -0700

    Update Page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from "@angular/router";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {MatCardModule, MatFormFieldModule, MatIconModule, MatSelectModule} from "@angular/material";
 
 const routes: Routes = [ // Array of all routes - modify when adding routes
     {path: '**', component: HomeComponent} // Default route
@@ -21,6 +23,11 @@ const routes: Routes = [ // Array of all routes - modify when adding routes
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        FormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatSelectModule,
         RouterModule.forRoot(routes)
     ],
     providers: [],
