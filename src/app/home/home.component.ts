c4af6310d41ff20434ab62eca3b8bcaa1ca07f4f
deb <debkbanerji@gmail.com>
2018-06-03 01:15:49 -0700

    Displaying Inputs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Http} from "@angular/http";
 import {HttpClient} from "@angular/common/http";
+import {type} from "os";
 
 @Component({
     selector: 'app-home',
@@ -8,6 +9,8 @@ import {HttpClient} from "@angular/common/http";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+    objectKeys = Object.keys;
 
     tableSizes: Array<string> = ['1d', '2d'];
     tableSize = this.tableSizes[1];
@@ -40,6 +43,32 @@ export class HomeComponent implements OnInit {
     transposeTable(): void {
         this.transpose2dTable = !this.transpose2dTable;
         // TODO: Rerun test cases
+    }
+
+    getDisplayedValue(value: any, type: string): string {
+        // console.log('type', type);
+        // console.log('value', JSON.stringify(value));
+        // return type + '_' + value.toString();
+        if (value === null || value === undefined) {
+            return '?';
+        }
+        if (type === 'int' || type === 'float') {
+            if (value === Number.MAX_SAFE_INTEGER || value === Number.MAX_VALUE || value === 'infinity') {
+                return '&#x221e;';
+            } else if (value === Number.MIN_SAFE_INTEGER || value === Number.MIN_VALUE || value === '-infinity') {
+                return '-&#x221e;';
+            } else {
+                if (type === 'float') {
+                    return value !== parseInt(value) ? value.toFixed(2) : Math.floor(value);
+                } else {
+                    return value;
+                }
+            }
+        } else if (type === ' boolean') {
+            return value ? '&#x2714;' : '&#x2718;';
+        } else {
+            return 'Unrecognized type';
+        }
     }
 
     range(_p: number, _t?: number, _s?: number): Array<number> {
