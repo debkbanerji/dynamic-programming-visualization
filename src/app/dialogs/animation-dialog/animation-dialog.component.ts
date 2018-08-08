d723bf3d7ca39bd16013f01e78f79d9bf9bbad33
deb <debkbanerji@gmail.com>
2018-08-08 10:08:58 +0530

    Animation dialog

---
+++ src/app/dialogs/animation-dialog/animation-dialog.component.ts
@@ -0,0 +1,18 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+    selector: 'app-confirmation-solution-dialog',
+    templateUrl: './confirmation-dialog.component.html'
+})
+export class ConfirmationDialogComponent {
+
+    constructor(
+        public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any) {
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+}
