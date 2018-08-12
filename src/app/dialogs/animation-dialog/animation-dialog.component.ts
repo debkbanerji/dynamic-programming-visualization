be6a3c026115df0813114d8d8cbd91d16e1f8f1c
deb <debkbanerji@gmail.com>
2018-08-12 14:48:42 +0530

    Formatting Update

--- src/app/dialogs/animation-dialog/animation-dialog.component.ts
+++ src/app/dialogs/animation-dialog/animation-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
-import {AnimationDataService} from "../../providers/animation-data.service";
+import {AnimationDataService} from '../../providers/animation-data.service';
 
 @Component({
     selector: 'app-animation-dialog',
--- src/app/dialogs/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/dialogs/confirmation-dialog/confirmation-dialog.component.ts
@@ -3,7 +3,8 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
     selector: 'app-confirmation-solution-dialog',
-    templateUrl: './confirmation-dialog.component.html'
+    templateUrl: './confirmation-dialog.component.html',
+    styleUrls: ['./confirmation-dialog.component.css']
 })
 export class ConfirmationDialogComponent {
 
